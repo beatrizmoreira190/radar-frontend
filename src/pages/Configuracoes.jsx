35f448e5810236ec34b2f18ff13a0676920fd904
beatrizmoreira190 <beatrizmoreira190@gmail.com>
2025-11-13 17:28:29 -0300

    Create Configuracoes.jsx

---
+++ src/pages/Configuracoes.jsx
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default function Configuracoes() {
+  return (
+    <Layout titulo="Configurações">
+      <p className="text-gray-600">Preferências e integração da conta.</p>
+    </Layout>
+  );
+}
