753d15dd3e834771c15cdecda400156652403d3e
beatrizmoreira190 <beatrizmoreira190@gmail.com>
2025-11-13 17:28:16 -0300

    Create Propostas.jsx

---
+++ src/pages/Propostas.jsx
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default function Propostas() {
+  return (
+    <Layout titulo="Propostas">
+      <p className="text-gray-600">Histórico de propostas enviadas.</p>
+    </Layout>
+  );
+}
