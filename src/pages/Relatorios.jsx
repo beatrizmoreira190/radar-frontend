9be29fbaf948e495887acc2d9c0bd76c045e380b
beatrizmoreira190 <beatrizmoreira190@gmail.com>
2025-11-13 17:28:03 -0300

    Create Relatorios component for report generation

---
+++ src/pages/Relatorios.jsx
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default function Relatorios() {
+  return (
+    <Layout titulo="Relatórios">
+      <p className="text-gray-600">Aqui você poderá gerar relatórios PDF/Excel.</p>
+    </Layout>
+  );
+}
