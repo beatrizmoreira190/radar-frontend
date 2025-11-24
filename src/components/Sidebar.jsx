21603ca9d6ca1b9b72dc2d5b3922a870be89ba30
beatrizmoreira190 <beatrizmoreira190@gmail.com>
2025-11-24 16:18:56 -0300

    Update Sidebar link text to 'Licitações PNCP'

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,7 +11,7 @@ export default function Sidebar() {
         </Link>
 
         <Link to="/licitacoes" className="hover:text-blue-600">
-          Licitações
+          Licitações PNCP
         </Link>
 
         {/* ⭐ NOVO ITEM ADICIONADO */}
