c5e487627c4f39a7f6015bb6c97d761b6adb1f5d
beatrizmoreira190 <beatrizmoreira190@gmail.com>
2025-11-24 18:43:49 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
+
 import Licitacoes from "./pages/Licitacoes";
 import LicitacaoDetalhe from "./pages/LicitacaoDetalhe";
 import Interesses from "./pages/Interesses";
 import AcompanhamentoLicitacao from "./pages/AcompanhamentoLicitacao";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
     <BrowserRouter>
       <div className="flex">
         
-
         <div className="flex-1">
           <Routes>
+            <Route path="/" element={<Dashboard />} />  {/* Dashboard como home */}
+            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/licitacoes" element={<Licitacoes />} />
-            <Route path="/" element={<Licitacoes />} />
             <Route path="/licitacao/:id" element={<LicitacaoDetalhe />} />
             <Route path="/interesses" element={<Interesses />} />
-            <Route path="/licitacao/:id" element={<LicitacaoDetalhe />} />
             <Route path="/acompanhamento/:id" element={<AcompanhamentoLicitacao />} />
-            <Route path="/dashboard" element={<Dashboard />} />
           </Routes>
         </div>
       </div>
