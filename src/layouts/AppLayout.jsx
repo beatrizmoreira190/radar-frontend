68c208f3108e995fd45a8f16132c0aeda6d3f387
beatrizmoreira190 <beatrizmoreira190@gmail.com>
2025-11-25 16:12:51 -0300

    Create AppLayout.jsx

---
+++ src/layouts/AppLayout.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../components/Sidebar";
+
+export default function AppLayout({ children }) {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="flex-1">
+        {children}
+      </div>
+    </div>
+  );
+}
